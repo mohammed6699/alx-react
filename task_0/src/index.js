2cab980bf6e06dfb903682d568250353bcf5bdbe
unknown <mohammedahmed.refaat@gmail.com>
2024-09-02 22:46:40 +0300

    task_0

---
+++ task_0/src/index.js
@@ -0,0 +1,6 @@
+import $ from "jQuery";
+$(document).ready(function () {
+    $('body').append('<p>Holberton Dashboard</p>')
+    $('body').append('<p>Dashboard data for the students</p>')
+    $('body').append('<p>Copyright - Holberton School</p>')
+});
