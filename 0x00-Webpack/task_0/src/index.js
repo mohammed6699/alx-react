89854136dc4ca9056ad2a969666a66e50f27022f
unknown <mohammedahmed.refaat@gmail.com>
2024-09-03 03:57:14 +0300

    task_0

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,7 +1,6 @@
-import $ from "jQuery";
-$(document).ready(function() {
+import $ from "jquery";
+
     // Your code here
     $('body').append('<p>Holberton Dashboard</p>');
     $('body').append('<p>Dashboard data for the students</p>');
     $('body').append('<p>Copyright - Holberton School</p>');
-});
