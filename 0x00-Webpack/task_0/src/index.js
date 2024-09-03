c042575616b516dce954d1142598fe7ab6add79f
unknown <mohammedahmed.refaat@gmail.com>
2024-09-03 04:06:04 +0300

    task_0 modified

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,6 +1,5 @@
 import $ from "jquery";
 
-    // Your code here
-    $('body').append('<p>Holberton Dashboard</p>');
-    $('body').append('<p>Dashboard data for the students</p>');
-    $('body').append('<p>Copyright - Holberton School</p>');
+$(body).append("<p>Holberton Dashboard</p>");
+$(body).append("<p>Dashboard data for the students</p>");
+$(body).append("<p>HCopyright - Holberton School</p>");
