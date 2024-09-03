05aa3cccf0083fa3d031c2a0476ac32b8b7107e8
unknown <mohammedahmed.refaat@gmail.com>
2024-09-03 03:16:38 +0300

    modifying

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -3,35 +3,36 @@
     const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
     module.exports = {
-    mode: 'production',
-    entry: './src/js/dashboard_main.js',
+    mode: 'production',  // Set mode to 'production' for optimized output
+    entry: './js/dashboard_main.js',  // Path to your entry file
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',  // Output filename
+        path: path.resolve(__dirname, 'public'),  // Output directory
     },
     module: {
         rules: [
         {
             test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
+            use: ['style-loader', 'css-loader'],  // Loaders for handling CSS files
         },
         {
             test: /\.(png|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
+            type: 'asset/resource',  // Loader for handling images
             generator: {
-            filename: 'images/[hash][ext][query]',
+            filename: 'images/[hash][ext][query]',  // Output path for images
             },
         },
         ],
     },
     plugins: [
-        new CleanWebpackPlugin(),
+        new CleanWebpackPlugin(),  // Plugin to clean output directory before each build
         new HtmlWebpackPlugin({
-        template: './src/index.html',
+        template: './public/index.html',  // Path to your HTML template
+        inject: 'body',  // Inject the script at the bottom of the body
         }),
     ],
     performance: {
-        maxAssetSize: 1000000, // Set a custom max asset size to avoid warnings
+        maxAssetSize: 1000000,  // Set custom asset size limit
         maxEntrypointSize: 1000000,
     },
     };
