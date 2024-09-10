ae174bd7370e69e57262d6be91c465915eafe73f
unknown <mohammedahmed.refaat@gmail.com>
2024-09-10 03:58:14 +0300

    all

--- 0x02-react_props/task_4/dashboard/src/App/App.js
+++ 0x02-react_props/task_4/dashboard/src/App/App.js
@@ -8,18 +8,23 @@ import Header from '../Header/Header.js'
 import Footer from '../Footer/Footer.js';
 import Login from '../Login/Login.js';
 import NotificationItem from '../Notifications/NotificationItem.js';
+import NotificationsTable from '../Notifications/NotificationsTable.js';
 function App() {
   return (
     <>
+    <NotificationItem />
       <Notifications />
       <div className='App'>
         <Header />  
       </div>
       <div className='line'></div>
-      <div className='.App-body'>
-        <Login />
-        <NotificationItem />
+      <div className='App-body'>
+        <Login/>
         </div>
+      <div className='line-body'></div>
+      <div className="App-body">
+        <NotificationsTable />
+      </div>
       <div className='line-body'></div>
       <div className='App-footer'>
         <Footer />
